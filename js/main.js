0ae1dc7cfc9619ce7594b932a08c3c8eca390326
Thankgod222 <ogbonnathankgod285@gmail.com>
2022-07-24 14:24:05 +0100

    done  carousel

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// NAVBAR
+
+
+// OWL CAROUSEL
+
+$(".owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 100,
+    items: 3,
+    nav: true,
+    loop: true
+});
