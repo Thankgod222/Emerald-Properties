0a53326cfdc41d9e883316a0a4cc9ac2a08c24d7
Thankgod222 <ogbonnathankgod285@gmail.com>
2022-07-26 03:21:35 +0100

    done hero property and agent section

--- js/main.js
+++ js/main.js
@@ -7,7 +7,33 @@ $(".owl-carousel").owlCarousel({
     autoplay: true,
     autoplayhoverpause: true,
     autoplaytimeout: 100,
-    items: 3,
+    items: 1,
     nav: true,
-    loop: true
+    loop: true,
+    animateIn: 'flipInY',
+    animateOut: 'zoomOutDown',
+
+    responsive: {
+        0: {
+            items: 1,
+            dots: false
+        },
+        485: {
+            items: 1,
+            dots: false
+        },
+        728: {
+            items: 2,
+            dots: false
+        },
+        960: {
+            items: 3,
+            dots: false
+        },
+        1200: {
+            items: 3,
+            dots: true
+        }
+
+    }
 });
