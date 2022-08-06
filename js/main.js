8647f8d7838accc6ef11c15bc6d1a2b3a6e20cdd
Thankgod222 <ogbonnathankgod285@gmail.com>
2022-08-06 07:45:26 +0100

    starting with contact pages

--- js/main.js
+++ js/main.js
@@ -37,3 +37,86 @@ $(".owl-carousel").owlCarousel({
 
     }
 });
+
+
+
+// BLOG owlCarousel
+
+/**
+ * News carousel
+ */
+new Swiper('.blog-slider', {
+    speed: 200,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    }
+});
+
+// Testimonial owlCarousel
+
+/**
+ * testimonial carousel
+ */
+new Swiper('.testimonial-slider ', {
+    speed: 200,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+
+        1200: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        }
+    }
+});
+
+
+/**
+   * Testimonial carousel
+   */
+new Swiper('#testimonial-carousel', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.testimonial-carousel-pagination',
+        type: 'bullets',
+        clickable: true
+    }
+});
