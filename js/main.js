7c25e8c9888cc95e68f34666bdf79525b2b26383
Thankgod222 <ogbonnathankgod285@gmail.com>
2022-08-07 02:42:49 +0100

    fixing media Queries

--- js/main.js
+++ js/main.js
@@ -38,6 +38,37 @@ $(".owl-carousel").owlCarousel({
     }
 });
 
+// Property owlCarousel
+
+/**
+ * Property carousel
+ */
+new Swiper('.property-slider', {
+    speed: 200,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    }
+});
+
 
 
 // BLOG owlCarousel
